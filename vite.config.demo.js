d32df60d4ecb36a139da594b49d8a2a225241d4e
Jim Gallagher <jegallagher@gmail.com>
2026-03-04 19:00:05 +0000

    fix root for assets

--- vite.config.demo.js
+++ vite.config.demo.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react()],
-    base: '/<REPO>/',
+    // base: '/<REPO>/',
     build: {
         outDir: 'demo'
     },
