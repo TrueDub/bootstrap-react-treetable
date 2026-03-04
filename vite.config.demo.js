a0297c717df24b18c3ca993fccf67932b841268e
Jim Gallagher <jegallagher@gmail.com>
2026-03-04 20:01:34 +0000

    fix base

--- vite.config.demo.js
+++ vite.config.demo.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react()],
-    base: '/<REPO>/',
+    base: '/bootstrap-react-treetable/',
     build: {
         outDir: 'demo'
     },
